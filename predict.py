402545458f1bdda7cf2646dcd92b4f3f9b65d8a7
chenxwh <chenxi.whitehouse@gmail.com>
2024-03-08 19:56:49 +0000

    add refiner

--- predict.py
+++ predict.py
@@ -4,8 +4,10 @@
 import os
 import subprocess
 import time
+from PIL import Image
 from cog import BasePredictor, Input, Path
 import torch
+from diffusers import ControlNetModel
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 from src.scheduler_perflow import PeRFlowScheduler
 
@@ -18,11 +20,12 @@ MODEL_URLS = {
     f"hansyan/perflow-sd15-{m}": f"https://weights.replicate.delivery/default/piecewise-rectified-flow/perflow-sd15-{m}.tar"
     for m in ["dreamshaper", "realisticVisionV51", "disney"]
 }
-
 MODEL_CKPTS = {
     f"hansyan/perflow-sd15-{m}": f"pretrained/perflow-sd15-{m}"
     for m in ["dreamshaper", "realisticVisionV51", "disney"]
 }
+CONTROLNET_URL = "https://weights.replicate.delivery/default/piecewise-rectified-flow/control_v11f1e_sd15_tile.tar"
+CONTROLNET_CKPT = "pretrained/control_v11f1e_sd15_tile"
 
 
 def download_weights(url, dest, extract=True):
@@ -40,8 +43,10 @@ class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         for model in MODELS:
-            if not os.path.exists(MODEL_URLS[model]):
+            if not os.path.exists(MODEL_CKPTS[model]):
                 download_weights(MODEL_URLS[model], MODEL_CKPTS[model])
+        if not os.path.exists(CONTROLNET_CKPT):
+            download_weights(CONTROLNET_URL, CONTROLNET_CKPT)
         self.pipelines = {
             model: StableDiffusionPipeline.from_pretrained(
                 MODEL_CKPTS[model], torch_dtype=torch.float16
@@ -52,7 +57,25 @@ class Predictor(BasePredictor):
             pipe.scheduler = PeRFlowScheduler.from_config(
                 pipe.scheduler.config, prediction_type="epsilon", num_time_windows=4
             )
-            pipe.to("cuda", torch.float16)
+            pipe.to("cuda")
+        self.controlnet = ControlNetModel.from_pretrained(
+            CONTROLNET_CKPT, torch_dtype=torch.float16
+        )
+
+        self.upscalers = {
+            model: StableDiffusionPipeline.from_pretrained(
+                MODEL_CKPTS[model],
+                torch_dtype=torch.float16,
+                custom_pipeline="stable_diffusion_controlnet_img2img",
+                controlnet=self.controlnet,
+            )
+            for model in MODELS
+        }
+        for upscaler in self.upscalers.values():
+            upscaler.scheduler = PeRFlowScheduler.from_config(
+                upscaler.scheduler.config, prediction_type="epsilon", num_time_windows=4
+            )
+            upscaler.to("cuda")
 
     @torch.inference_mode()
     def predict(
@@ -64,7 +87,7 @@ class Predictor(BasePredictor):
         ),
         prompt: str = Input(
             description="Input prompt",
-            default="A man with brown skin, a beard, and dark eyes",
+            default="A colorful bird standing on the tree, open beak",
         ),
         negative_prompt: str = Input(
             description="Specify things to not see in the output",
@@ -79,12 +102,17 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
+        use_refiner: bool = Input(
+            description="Combine with ControlNet-Tile to upscale to 1024x1024",
+            default=False,
+        ),
     ) -> Path:
         """Run a single prediction on the model"""
         if seed is None:
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
         pipe = self.pipelines[model]
+        upscaler = self.upscalers[model] if use_refiner else None
 
         generator = torch.Generator("cuda").manual_seed(seed)
         samples = pipe(
@@ -96,6 +124,33 @@ class Predictor(BasePredictor):
             guidance_scale=guidance_scale,
             generator=generator,
         ).images
-        out_path = "/tmp/out.png"
-        samples[0].save(out_path)
+        out_path = "/tmp/output.png"
+        if not use_refiner:
+            samples[0].save(out_path)
+        else:
+            condition_image = resize_for_condition_image(samples[0], 1024)
+            refined_samples = upscaler(
+                prompt="best quality",
+                negative_prompt="blur, lowres, bad anatomy, bad hands, cropped, worst quality",
+                image=condition_image,
+                controlnet_conditioning_image=condition_image,
+                width=condition_image.size[0],
+                height=condition_image.size[1],
+                strength=1.0,
+                generator=torch.manual_seed(seed),
+                num_inference_steps=4,
+            ).images
+            refined_samples[0].save(out_path)
         return Path(out_path)
+
+
+def resize_for_condition_image(input_image: Image, resolution: int):
+    input_image = input_image.convert("RGB")
+    W, H = input_image.size
+    k = float(resolution) / min(H, W)
+    H *= k
+    W *= k
+    H = int(round(H / 64.0)) * 64
+    W = int(round(W / 64.0)) * 64
+    img = input_image.resize((W, H), resample=Image.LANCZOS)
+    return img
